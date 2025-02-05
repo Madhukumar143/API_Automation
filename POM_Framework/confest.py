ea310bc2661fc088374f4717b76ad0d49f745b4f
Madhukumar1718 <madhukumar.hm@terralogic.com>
2025-02-05 12:59:27 +0530

    directory issue resolved

--- POM_Framework/confest.py
+++ POM_Framework/confest.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 import pytest
 
 @pytest.hookimpl(tryfirst=True)
--- POM_Framework/tests/test_getuser.py
+++ POM_Framework/tests/test_getuser.py
@@ -1,6 +1,5 @@
 import json
 import pytest
-import requests
 from POM_Framework.utils.api import APIS
 
 @pytest.fixture(scope="module")
@@ -13,4 +12,4 @@ def test_getuser_validation(common):
     formatted_json = json.dumps(data, indent=4)
     print("\n",formatted_json)
     assert response.status_code == 200, "Status code did not match"
-    assert len(response.json())<0,"response does not contain any data"
+    assert len(response.json())>0,"response does not contain any data"
