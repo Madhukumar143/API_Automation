2c82b2ff449b71fe6934e6a72d255eee455d03ff
Madhukumar1718 <madhukumar.hm@terralogic.com>
2025-02-05 12:12:27 +0530

    POM Framework set up done

---
+++ POM_Framework/confest.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+import pytest
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    report_dir = "reports"
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    config.option.htmlpath = f"{report_dir}/reports_{now}.html"
+
+@pytest.fixture()
+def setup_teardown():
+    print("Starting")
+    yield
+    print("End")
---
+++ POM_Framework/tests/test_getuser.py
@@ -0,0 +1,16 @@
+import json
+import pytest
+import requests
+from POM_Framework.utils.api import APIS
+
+@pytest.fixture(scope="module")
+def common():
+    return APIS()
+
+def test_getuser_validation(common):
+    response = common.get('users')
+    data = response.json()
+    formatted_json = json.dumps(data, indent=4)
+    print("\n",formatted_json)
+    assert response.status_code == 200, "Status code did not match"
+    assert len(response.json())<0,"response does not contain any data"
